2d263fe5cdfd25203a069f5dd4fcd1478071d9ea
c1m50c <terminalityms@gmail.com>
2021-12-05 20:06:57 -0600

    Restart 🔃

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,3 @@
-use term_painter::{Color::*, ToStyle};
-use std::io::{self, Write};
-
-mod tokenizer;
-
-
-/// Gets user input from the terminal, prompt hardcoded.
-fn get_input() -> String {
-    let mut input: String = String::new();
-
-    /* Prompt */
-    print!("{}{}",
-        BrightMagenta.bold().paint("evalulator"),
-        BrightYellow.bold().paint(":$ "),
-    );
-
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut input)
-        .expect("Failed to read line.");
-    return input.trim().to_string();
-}
-
-
 fn main() {
-    loop {
-        let inp = get_input();
-        if inp.is_empty() { return; }
-
-        let mut tokenizer = tokenizer::Tokenizer::new(inp);
-    }
+    println!("Hello, world!");
 }
