81101cd8c00b81f7622226e17d570fa12e730bc6
c1m50c <terminalityms@gmail.com>
2021-12-06 18:44:32 -0600

    `Expression`, `ArithmeticOperation` ✔

--- src/parser/enums.rs
+++ src/parser/enums.rs
@@ -1,13 +1,47 @@
+use std::boxed::Box;
 use std::fmt;
 
 
 #[derive(Debug)]
 pub enum Statement {
+    Arithmetic(Expression),
+}
 
+
+#[derive(Debug)]
+pub enum ArithmeticOperation {
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+}
+
+
+#[derive(Debug)]
+pub enum Expression {
+    /* Mathematical */
+    Arithmetic(Box<Expression>, ArithmeticOperation, Box<Expression>),
+    
+    /* Types */
+    Float(f64),
 }
 
 
 impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        return write!(f, "{:?}", self);
+    }
+}
+
+
+impl fmt::Display for ArithmeticOperation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        return write!(f, "{:?}", self);
+    }
+}
+
+
+impl fmt::Display for Expression {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         return write!(f, "{:?}", self);
     }
