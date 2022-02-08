ce24bb3e06d804054febe920bd8e60141cd274f6
c1m50c <terminalityms@gmail.com>
2022-02-07 21:02:54 -0600

    Begin on rewrite ✏

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,12 @@
-mod parser;
-mod lexer;
-mod shell;
-
-
 #[allow(unused_imports)]
 use term_painter::{ToStyle, Color::*};
-
-use shell::eval::evaluate_statements;
-use shell::{input, print_debug};
-use parser::Parser;
-use lexer::Lexer;
-
 use std::thread;
 
 
 fn main() {
     loop {
-        let inp: String = input();
-        if inp.is_empty() { continue; }
-
         let execution_thread = thread::spawn(move || {
-            let mut lexer: Lexer = Lexer::new(inp.clone());
-            let mut parser: Parser = Parser::new(lexer.clone());
-            let parsed_data = parser.parse();
-
-            if cfg!(debug_assertions) {
-                while let Some(t) = lexer.next() {
-                    print_debug("Lexical Token", format!("{}", t).as_ref());
-                }
-
-                for s in parsed_data.clone() {
-                    print_debug("Parsing Statement", format!("{}", s).as_ref());
-                }
-            }
-
-            evaluate_statements(parsed_data);
+            if cfg!(debug_assertions) {}
         });
 
         let _ = execution_thread.join();
