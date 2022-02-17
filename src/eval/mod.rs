37413d2a06d88a2a310e63f7b83be050ed9a29a0
c1m50c <terminalityms@gmail.com>
2022-02-17 16:41:26 -0600

    Evaluation Cleanup 🧹

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -1,18 +1,33 @@
 use super::shell::{ShellError, shell_panic};
 use super::parser::enums::Statement;
+use core::result::Result;
 use std::process::exit;
+
+
+#[allow(unreachable_code)]
+fn evaluate_command(command: &str) -> Result<(), String> {
+    match command {
+        "quit" | "exit" => exit(0),
+
+        c => return Err(
+            format!("Statement::Command({:?}) cannot be evaluated, command does not exist.", c)
+        ),
+    }
+    
+    return Ok(());
+}
 
 
 #[allow(unreachable_patterns)]
 fn evaluate_statement(statement: Statement) {
     match statement {
-        Statement::Command(c) => match c.to_lowercase().as_str() {
-            "quit" | "exit" => exit(0),
-
-            c => shell_panic(
-                ShellError::EvaluationError,
-                format!("Statement::Command({:?}) cannot be evaluated, command '{}' does not exist.", c, c)
-            ),
+        Statement::Command(c) => {
+            if let Err(error_message) = evaluate_command(c.to_lowercase().as_str()) {
+                shell_panic(
+                    ShellError::EvaluationError,
+                    error_message
+                );
+            }
         },
     
         s => shell_panic(
