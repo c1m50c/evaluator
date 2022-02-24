0e1d2b7be3d444a4c7202de84e849c0488f5f3ca
c1m50c <terminalityms@gmail.com>
2022-02-23 18:57:59 -0600

    Chain `Statement::Arithmetic` Parsing 🧮

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -28,6 +28,11 @@ impl Parser {
         
         return Statement::Number(result);
     }
+
+    /// Pops the last [`Statement`] within the `statements` field, panicking if no [`Statement`]s are contained within the field.
+    fn pop_statement(&mut self) -> Statement {
+        return self.statements.pop().unwrap();
+    }
 }
 
 
@@ -96,6 +101,38 @@ impl Parser {
                 Token::Word(word) => {
                     Statement::Command(word.to_lowercase())
                 },
+
+                // TODO: Cleanup using the `Clone` method if applicable.
+                t if t.clone().is_arithmetic_operator() => {
+                    let maybe_arithmetic = self.pop_statement();
+
+                    if let Statement::Arithmetic(_, _, _) = maybe_arithmetic {
+                        let operator = t.clone().try_into()
+                            .unwrap_or_else(|err| shell_panic(
+                                err,
+                                format!("Cannot convert Token::{:?} into a BinaryOperation", t)
+                            ));
+                        
+                        if let Some(Token::Number(b)) = self.lexer.get_token() {
+                            let b = Self::parse_number(b);
+                            Statement::Arithmetic(Box::new(maybe_arithmetic), operator, Box::new(b))
+                        }
+                        
+                        else {
+                            shell_panic(
+                                ShellError::SyntaxError,
+                                format!("Expected a number after Token::{:?}.", operator)
+                            );
+                        }
+                    }
+
+                    else {
+                        shell_panic(
+                            ShellError::EvaluationError,
+                            format!("Expected an Arithmetic Statement before Token::{:?}.", t)
+                        );
+                    }
+                }
                 
                 t => shell_panic(
                     ShellError::ParsingError,
